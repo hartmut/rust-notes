8c5ff9455bd5bb4ac2b864619732f8bf9c4a512c
hartmut <hartmut.prochaska@gmail.com>
2016-12-31 15:03:24 +0100

    rm target compile

--- code/strings/target/debug/deps/fileoperationstests-5d11ae3fc32fed4a.d
+++
@@ -1,5 +0,0 @@
-/home/freeze/projects/rust-notes/code/strings/target/debug/deps/fileoperationstests-5d11ae3fc32fed4a: tests/fileoperationstests.rs
-
-/home/freeze/projects/rust-notes/code/strings/target/debug/deps/fileoperationstests-5d11ae3fc32fed4a.d: tests/fileoperationstests.rs
-
-tests/fileoperationstests.rs:
