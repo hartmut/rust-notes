c37de830ccaed9ca57c1139b01a959b24054fad1
hartmut <hartmut.prochaska@gmail.com>
2017-06-11 14:19:12 +0200

    further tests with references

--- code/gist/reftest.rs
+++ code/gist/reftest.rs
@@ -3,6 +3,11 @@ struct Astruct {
     vala: i32,
 }
 
+#[derive(Debug)]
+struct Bstruct<'a> {
+    revvala: &'a Astruct,
+}
+
 fn test1(b: &Astruct) {
     println!("test1 before mut {:?}", b);
 }
@@ -15,9 +20,16 @@ fn testmut(b: &mut Astruct) {
     b.vala = 24;
 }
 
+fn save_in_structure(b: &Astruct) -> Bstruct {
+    let c: Bstruct = Bstruct { revvala: b };
+    c
+}
+
 fn main() {
     let mut a: Astruct = Astruct { vala: 42 };
     test1(&a);
     testmut(&mut a);
     test2(&a);
+    let mut b = save_in_structure(&a);
+    println!("Structure with reference {:?}", b);
 }
