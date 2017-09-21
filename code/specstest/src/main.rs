8441d094cec6c09ac8c997a30f11dca2982c5e9a
hartmut <hartmut.prochaska@gmail.com>
2017-09-21 16:46:58 +0200

    specstest

--- code/specstest/src/main.rs
+++ code/specstest/src/main.rs
@@ -1,3 +1,36 @@
+extern crate specs;
+// extern crate specs_derive;
+
+// #[macro_use]
+
+use specs::{Component, VecStorage};
+use specs::World;
+// use specs_derive;
+
+#[derive(Debug)]
+// #[derive(Component,Debug)]
+// #[component(VecStorage)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+impl Component for Position {
+    type Storage = VecStorage<Self>;
+}
+
+#[derive(Debug)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
+impl Component for Velocity {
+    type Storage = VecStorage<Self>;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut world = World::new();
+    world.register::<Position>();
+    world.register::<Velocity>();
 }
