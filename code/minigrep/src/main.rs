770bda09f6ba15e2a1c525c6d7e5faff6cbea4db
hartmut <hartmut.prochaska@gmail.com>
2018-11-25 16:35:38 +0100

    experiments

--- code/minigrep/src/main.rs
+++ code/minigrep/src/main.rs
@@ -1,22 +1,24 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use minigrep;
+use minigrep::Config;
 
 // rust learning
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {:?}", query);
-    println!("in file {:?}", filename);
+    println!("Searching for {:?}", config.query);
+    println!("in file {:?}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error {}", e);
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    };
 }
--- code/serdejson/src/main.rs
+++ code/serdejson/src/main.rs
@@ -3,48 +3,44 @@
 
 // extern
 extern crate serde_derive;
-extern crate serde_json;
 extern crate serde;
+extern crate serde_json;
 
 // mods
-mod common;
 mod aspects;
+mod common;
 
 // uses
 use std::collections::HashMap;
 
-
-
 // all the elemenets
 // updates from https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json
 
-
 fn main() {
-
     // create easy examplefile, how does it look?
     aspects::elementreading::create_example();
 
     // ## Sequence start
     // first version naive interpretation
     let elementlist = aspects::elementreading::read_elementlist_file();
-    println!{"{:?} \n", elementlist};
+    println! {"{:?} \n", elementlist};
     // access submap
     let ehash = String::from("elements");
     let evalue = elementlist.get(&ehash).unwrap();
-    println!{"{:?} \n", evalue};
+    println! {"{:?} \n", evalue};
     // print first value of Vector
-    println!{"{:?} \n", evalue[0]};
+    println! {"{:?} \n", evalue[0]};
     // ## Sequence stop
 
     // second version by HashMap
     let elementlist = aspects::elementreading::read_elementlist_file_by_hashmap();
-    println!{"{:?} \n", elementlist};
+    println! {"{:?} \n", elementlist};
 
     // third version, automatic import of full file, by using testout1.json
-    println!("Elementlist in one reading \n", );
+    println!("Elementlist in one reading \n",);
     let e = aspects::elementreading::read_elementlist_file_at_once();
     println!("{:?}", e);
-    println!("works, but not with the original file, because of null values and empty f64 values", );
+    println!("works, but not with the original file, because of null values and empty f64 values",);
 
     // fourth version, does it get better with using deserialize_with?
     // yes it works
@@ -52,5 +48,4 @@ fn main() {
     let e = aspects::elementreading::read_elementlist_file_and_resolve_nulls();
     println!("{:?}\n\n\n", e);
     println!("{:?}\n", e[5]);
-
 }
