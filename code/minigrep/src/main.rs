cc1cf69cb3ff3b1263c097cfc9da8b1414ccd1b3
hartmut <hartmut.prochaska@gmail.com>
2018-11-27 21:57:17 +0100

    rust book chapter 12

--- code/minigrep/src/main.rs
+++ code/minigrep/src/main.rs
@@ -9,15 +9,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {:?}", config.query);
-    println!("in file {:?}", config.filename);
+    // println!("Searching for {:?}", config.query);
+    // println!("in file {:?}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error {}", e);
+        eprintln!("Application Error {}", e);
 
         process::exit(1);
     };
