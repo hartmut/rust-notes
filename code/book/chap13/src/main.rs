2c9ecd2b7dd3cf8e0d8a72068cae39dcd230e222
hartmut <hartmut.prochaska@gmail.com>
2018-12-17 13:00:11 +0100

    working with closures

--- code/book/chap13/src/main.rs
+++ code/book/chap13/src/main.rs
@@ -1,22 +1,54 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulate_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly ....");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_numer: u32) {
-    let expensive_result = simulate_expensive_calculation(intensity);
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly ....");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result);
-        println!("Next, do {} situps!", expensive_result);
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_numer == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes", expensive_result);
+            println!(
+                "Today, run for {} minutes",
+                expensive_result.value(intensity)
+            );
         }
     }
 }
@@ -26,4 +58,23 @@ fn main() {
     let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO Try modifying Cacher to hold a hash map rather than a single value
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+        assert_eq!(v2, 2);
+    }
+
+    // TODO We might want to cache the results of closures that take a string slice and
+    // return usize values, for example. To fix this issue, try introducing more generic
+    // parameters to increase the flexibility of the Cacher functionality.
+
 }
